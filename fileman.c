b2c7c3a80d877139dd0db14b032ad75ac80c07c5
Ivan Chen <ivanchen07@gmail.com>
2024-01-05 14:51:10 -0500

    Add functions + started work on custom ls + started working on recursive size

---
+++ fileman.c
@@ -0,0 +1,46 @@
+#include "network.h"
+
+int size(char* name, char recursive) {
+    struct stat stats;
+    if (recursive) {
+        int sum;
+        struct dirent *entry;
+        DIR *dir = opendir(name);
+        while (entry = readdir(dir)) {
+            if (entry->d_type == DT_DIR)
+                size(entry->d_name, 1);
+            else
+                size(entry->d_name, 0);
+        }
+    }
+
+    stat(name, &stats);
+    return stats.st_size;
+}
+
+int ls() {
+    struct dirent * entry;
+    DIR *dir = opendir(".");
+    while (entry = readdir(dir)) {
+        printf("%s %7d %12s\n",
+               entry->d_type == DT_DIR ? "d" : "f",
+               size(entry->d_name, 0),
+               entry->d_name);
+    }
+}
+
+int cd() {
+
+}
+
+int pwd() {
+
+}
+
+int rm() {
+
+}
+
+int main(int argc, char *argv[]) {
+    ls();
+}
